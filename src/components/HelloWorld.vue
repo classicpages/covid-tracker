30fd793bb86e8d4415ed7a3c52ef73b997c46708
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-05-27 10:50:22 +0000

    Initial setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-red-600 text-6xl">Hello World</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{vue,jsx,tsx,html,js}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
